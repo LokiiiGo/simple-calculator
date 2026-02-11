50a84e9764909f4ccc60d658d0ef073137586595
LokiiiGo <lucascalle880@gmail.com>
2026-02-11 12:44:40 -0300

    Fazendo alterações no arquivo page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,50 +41,50 @@ export default function Home() {
   };
   return (
     <div className="p-4 flex flex-col min-h-screen items-center justify-center bg-gray-900 text-white">
-  <h1 className="text-2xl md:text-3xl mt-5 font-bold">Simple Calculator</h1>
-  
-  <hr className="border-t border-white w-full max-w-xs md:max-w-md mx-auto mb-6 mt-5"></hr>
+      <h1 className="text-2xl md:text-3xl mt-5 font-bold">Simple Calculator</h1>
 
-  <div className="w-full max-w-md p-6 md:p-10 bg-amber-400 border-amber-50 border-2 rounded-2xl text-xl md:text-2xl font-bold text-center">
-    <span className="block mb-4">Operações Matemáticas</span>
-    
-    <div className="mt-6 text-left">
-      <label htmlFor="numeroInput1" className="block mb-2">Número 1:</label>
-      <input 
-        type="number" 
-        id="numeroUm" 
-        className="w-full text-black border-black bg-white p-3 rounded-md focus:ring-2 focus:ring-black outline-none" 
-        value={numero1} 
-        onChange={(e) => setNumero1(e.target.value)} 
-      />
+      <hr className="border-t border-white w-full max-w-xs md:max-w-md mx-auto mb-6 mt-5"></hr>
+
+      <div className="w-full max-w-md p-6 md:p-10 bg-amber-400 border-amber-50 border-2 rounded-2xl text-xl md:text-2xl font-bold text-center">
+        <span className="block mb-4">Operações Matemáticas</span>
+
+        <div className="mt-6 text-left">
+          <label htmlFor="numeroInput1" className="block mb-2">Número 1:</label>
+          <input
+            type="number"
+            id="numeroUm"
+            className="w-full text-black border-black bg-white p-3 rounded-md focus:ring-2 focus:ring-black outline-none"
+            value={numero1}
+            onChange={(e) => setNumero1(e.target.value)}
+          />
+        </div>
+
+        <div className="mt-6 text-left">
+          <label htmlFor="numeroInput2" className="block mb-2">Número 2:</label>
+          <input
+            type="number"
+            id="numeroDois"
+            className="w-full text-black border-black bg-white p-3 rounded-md focus:ring-2 focus:ring-black outline-none"
+            value={numero2}
+            onChange={(e) => setNumero2(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="mt-8 p-4 bg-amber-400 rounded-lg  border-amber-50 border-2 w-full max-w-md">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
+          <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('+')}>+</button>
+          <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('-')}>-</button>
+          <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('*')}>*</button>
+          <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('/')}>/</button>
+        </div>
+      </div>
+
+      <div className="mt-8 p-6 bg-amber-400 rounded-lg font-bold text-2xl  border-amber-50 border-2 w-full max-w-md text-center">
+        <p className="text-black">Resultado: {resultado}</p>
+      </div>
+
+      <footer className="mt-10 text-white">&copy; {new Date().getFullYear()} <a href='https://github.com/LokiiiGo/simple-calculator'>Simple Calculator</a></footer>
     </div>
-
-    <div className="mt-6 text-left">
-      <label htmlFor="numeroInput2" className="block mb-2">Número 2:</label>
-      <input 
-        type="number" 
-        id="numeroDois" 
-        className="w-full text-black border-black bg-white p-3 rounded-md focus:ring-2 focus:ring-black outline-none" 
-        value={numero2} 
-        onChange={(e) => setNumero2(e.target.value)} 
-      />
-    </div>
-  </div>
-
-  <div className="mt-8 p-4 bg-amber-400 rounded-lg  border-amber-50 border-2 w-full max-w-md">
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
-      <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('+')}>+</button>
-      <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('-')}>-</button>
-      <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('*')}>*</button>
-      <button className="bg-white text-black py-4 rounded-md font-bold text-2xl cursor-pointer hover:bg-gray-200 transition-colors" onClick={() => handleCalcular('/')}>/</button>
-    </div>
-  </div>
-
-  <div className="mt-8 p-6 bg-amber-400 rounded-lg font-bold text-2xl  border-amber-50 border-2 w-full max-w-md text-center">
-    <p className="text-black">Resultado: {resultado}</p>
-  </div>
-
-  <footer className="mt-10 text-white">&copy; {new Date().getFullYear()} <a href='https://github.com/LokiiiGo/simple-calculator'>Simple Calculator</a></footer>
-</div>
   );
 }
